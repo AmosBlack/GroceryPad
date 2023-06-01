0a08f014a1a073b44c6bf41fef41c5b9096474d4
AmosBlack <ojasmittal2008@gmail.com>
2023-06-01 20:21:13 +0530

    highlight feature

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
+//firebase initialization
 const appSettings = {
     databaseURL: "https://scrimfirebase-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }   
+const app = initializeApp(appSettings)
 
 //db
-const app = initializeApp(appSettings)
 const database = getDatabase(app)
 let shoppingListInDB    
 let shoppers = ref(database,`users`)
@@ -19,12 +20,15 @@ const loginButtonEl = document.getElementById("login-button")
 const userInputEl = document.getElementById("input-user")
 const loginToggle = document.getElementById("toggle")
 
+//login system 
 loginButtonEl.addEventListener("click", function(){
     //login system input, new db location   
     let userValue = userInputEl.value
     let shopperInDB = ref(database,`user/${userValue}`)
     clearInputFieldEl()
+    //update list value + login function
     onValue(shopperInDB,function(snapshot){
+        //signin/signup function        
         if(snapshot.exists()){
             shoppingListInDB = ref(database,`user/${userValue}`)
             
@@ -38,13 +42,13 @@ loginButtonEl.addEventListener("click", function(){
                 alert("Your account doesnt exist")
             }
         }
-
-        //signin/signup function
+        //store values from shopping list as array
         let itemsArray = Object.entries(snapshot.val())  
-
+        //empty shopping list to avoid duplication - each update introduces new values
         clearShoppingListEl()      
     
         for (let i = 0; i < itemsArray.length; i++) {
+            //for each item retrieved from db add to the list
             let currentItem = itemsArray[i]              
             appendItemToShoppingListEl(currentItem,userValue)
         }    
@@ -54,11 +58,10 @@ loginButtonEl.addEventListener("click", function(){
 addButtonEl.addEventListener("click", function() {
     //push input item to db
     let inputValue = inputFieldEl.value    
-    if(inputValue != ""){
+    if(inputValue != ""){ //ensure string isnt empty
         push(shoppingListInDB, inputValue)    
     }
     clearInputFieldEl()
-    //seems to work
 })
 
 function clearShoppingListEl() {
@@ -87,10 +90,13 @@ function appendItemToShoppingListEl(item,user) {
     newEl.addEventListener("click", function(){
         //change it from remove+add new el to updating in database with ID
         inputFieldEl.value = itemValue
+        newEl.style.backgroundColor = "#e6c178"
         addButtonEl.addEventListener("click",function(){
         removeListEl(user,itemID)
-
         })
+        setTimeout(()=>{
+            newEl.style.backgroundColor = "#FFFDF8"
+        },500)
 
     })
 
